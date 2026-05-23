57e2a771259b38c60e54988f3f30fc2c72fedcac
Nsanzimana emmanuel <emmanuelnsanzimana342@gmail.com>
2026-05-23 17:31:26 +0200

    Fix frontend subfolder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-red-500">
+        Tailwind CSS Working
+      </h1>
+    </div>
+  );
+}
+
+export default App;
